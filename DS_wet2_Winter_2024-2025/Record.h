edbfc24d332743799af1ce9dc8e980bf2650d8ff
guy friedman <guy.friedman2001@gmail.com>
2025-01-27 10:19:44 +0200

    הוספת קונסטרקטור ודסטרקטור לרקורד

--- DS_wet2_Winter_2024-2025/Record.h
+++ DS_wet2_Winter_2024-2025/Record.h
@@ -6,12 +6,25 @@
 #ifndef RECORD_H
 #define RECORD_H
 #include "ChainHashArray.h"
-
-typedef int what_to_hold;
+#include "uptree_jockeys.h"
+typedef TeamNode what_to_hold;
 
 class Record {
 protected:
-    ChainHashArray<what_to_hold> held_items;
+    ChainHashArray<what_to_hold>* held_items;
+    int record_value;
+public:
+    Record() = delete;
+
+    Record(int record_value) : record_value(record_value) {
+        this->held_items = new ChainHashArray<what_to_hold>();
+    }
+
+    ~Record() {
+        this->held_items->clear();
+        delete this->held_items;
+    }
+
 
 
 };
